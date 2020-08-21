437a471139190bcacaaea5a3528731bc34eedfd3
oswaldo <vnkvp@outlook.com>
2020-08-21 08:26:05 -0300

    Sidebar

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>twitter</h1>
+
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* feed */}
+      
+      {/* widgets */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Sidebar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Sidebar.css';
+import TwitterIcon from '@material-ui/icons/Twitter';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            {/* icon */}
+            <TwitterIcon />
+            {/* icon */}
+            {/* icon */}
+            {/* icon */}
+            {/* icon */}
+            {/* icon */}
+            {/* icon */}
+        </div>
+    )
+}
+
+export default Sidebar;
