77ea5a87241478244099548b71f6aa3964f19ee6
oswaldo <vnkvp@outlook.com>
2020-08-29 11:14:00 -0300

    setting-widgets

--- src/Post.js
+++ src/Post.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import './assets/Post.css'
 import VerifiedUserIcon from '@material-ui/icons/VerifiedUser'
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import PublishIcon from '@material-ui/icons/Publish';
 import { Avatar } from '@material-ui/core'
 
 function Post({
@@ -22,19 +26,28 @@ function Post({
                 <div className='postHeader'>
                     <div className='postHeaderText'>
                         <h3>
-                            oswaldo {" "}
+                            Oswaldo Inácio
+                            {" "}
                             <span>
-                                <VerifiedUserIcon fontSize='small' className='postBadge' />
+                                <VerifiedUserIcon className='postBadge' />
+                            </span>
+                            {" "}
+                            <span className='postHeaderSpecial'>
+                                {"@vnkvp"}
                             </span>
                         </h3>
                     </div>
                     <div className='postHeaderDescription'>
-                        <p>hello</p>
+                        <p>Hello</p>
                     </div>
                     <img
                         src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.redd.it%2F3l02rhocss051.jpg&f=1&nofb=1'
                         alt='strong-doge' className='postImage' />
                     <div className='postFooter'>
+                        <ChatBubbleOutlineIcon fontSize='small' />
+                        <RepeatIcon fontSize='small' />
+                        <FavoriteBorderIcon fontSize='small' />
+                        <PublishIcon fontSize='small' />
                     </div>
                 </div>
             </div>
